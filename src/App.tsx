ee3ac0213f660b7895e2b46187aa6237ea1d82bf
DiilaNa <liyanaarachchidilan@gmail.com>
2025-09-29 09:32:57 +0530

    Input Field Focus

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const App = () => {
   const ref = useRef("hello");
   console.log(ref);
+  console.log(ref.current); 
+  
 
-  const handleClick = () => {
-    ref.current = 
-    }
+  // useEffect(()=>{
+  //   console.log("Component is re-rendered");
+    
+  // })
+
+  // const handleClick = () => {
+  //   ref.current += 1
+  // }
+  const inputRef = useRef<any>(null)
+  const handleClick = () =>{
+    inputRef?.current.focus() 
+    inputRef.current.style.backgroundColor = "red"
+    
+  }
+    
   return <div>
+    <input ref={inputRef} />
     <button onClick={handleClick}>Click</button>
   </div>;
 }
