7c7a217f93a78a436dbfd72c78734782051a1a80
DiilaNa <liyanaarachchidilan@gmail.com>
2025-09-29 11:02:21 +0530

    Use Reducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -69,39 +69,96 @@
 // export default App
 
 
-// ---------------------------Use Call baxk---------------------------------
-import { useEffect, useState,useCallback} from "react"
+// // ---------------------------Use Call baxk---------------------------------
+// import { useEffect, useState,useCallback} from "react"
 
-const App = () =>{
-  const [name,setName] = useState("")
-   const[email,setEmail] = useState("")
-   const[result,setResult] = useState("")
+// const App = () =>{
+//   const [name,setName] = useState("")
+//    const[email,setEmail] = useState("")
+//    const[result,setResult] = useState("")
 
 
-  //  component rerender - function is recreated
-  //  const myFunction = () => {
-  //   console.log("Function is called");
-  //   return "hello"
+//   //  component rerender - function is recreated
+//   //  const myFunction = () => {
+//   //   console.log("Function is called");
+//   //   return "hello"
     
-  //  }
+//   //  }
 
-  const myFunction = useCallback(() => {
-    console.log("Function is running");
-    return "hello"
-  },[])
+//   const myFunction = useCallback(() => {
+//     console.log("Function is running");
+//     return "hello"
+//   },[])
 
-   useEffect(() => {
-    console.log("use effect is called");
-    setResult(myFunction())},[myFunction])
+//    useEffect(() => {
+//     console.log("use effect is called");
+//     setResult(myFunction())},[myFunction])
 
-    return (
-      <div>
-       <input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
-       <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <p>{result}</p>
-      </div>
-    )
+//     return (
+//       <div>
+//        <input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+//        <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+//         <p>{result}</p>
+//       </div>
+//     )
 
+// }
+
+// export default App
+
+// -------------------------------use Reducer----------------------------
+
+// {
+//   id: ""
+//   name: ""
+//   email: ""
+//   age: 0
+// }
+
+import { useReducer } from 'react'
+
+const reducer = (state:any,action:any)=>{
+  switch(action.type){
+    case "ID": return {...state,id:action.data}
+    // Samething is done by one line
+    // return{
+    //   id: action.data,
+    //   name:state.name,
+    //   email:state.email,
+    //   age:state.age
+
+    // }
+    case "NAME": 
+      return{...state,name:action.data}
+    case "EMAIL":
+      return{...state,email:action.data}
+    case "AGE":
+      return{...state,age:action.data}
+    default:
+      return state
+  }
+}
+// ...state - prevent deleting old data
+const App = () =>{
+const[state,dispatch] = useReducer(reducer,{
+  id:"",
+  name:"",
+  email:"",
+  age:""
+})
+
+console.log(state);
+
+
+  return(
+  <div>
+    <input placeholder="Id" value={state.id} onChange={(e)=>{dispatch({type: "ID",data:e.target.value})}}/>
+    <input placeholder="Name" value={state.name} onChange={(e)=>{dispatch({type: "NAME",data:e.target.value})}}/>
+    <input placeholder="Email" value={state.email} onChange={(e)=>{dispatch({type: "EMAIL",data:e.target.value})}}/>
+    <input placeholder="Age" value={state.age} onChange={(e)=>{dispatch({type: "AGE",data:e.target.value})}}/>
+  </div>
+  )
+  
 }
 
 export default App
