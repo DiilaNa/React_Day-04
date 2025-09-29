d30625952287a6846efceda2ae99acca5dcf4f6c
DiilaNa <liyanaarachchidilan@gmail.com>
2025-09-29 10:23:27 +0530

    Use Call back

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import { useEffect, useRef } from "react";
 
-import { useMemo, useState } from "react"
+
 
 // const App = () => {
 //   const ref = useRef("hello");
@@ -33,36 +33,75 @@ import { useMemo, useState } from "react"
 // export default App;
 
 
-// --------------------------useMemo-----------------------------------
+// // --------------------------useMemo-----------------------------------
+// import { useMemo, useState } from "react"
 
-const inputHandler = (value: any) =>{
+// const inputHandler = (value: any) =>{
 
-  let sum = 0
-  for (let i = 0; i < 1000000000; i++) {
-    sum++
+//   let sum = 0
+//   for (let i = 0; i < 1000000000; i++) {
+//     sum++
     
-  }
-  return value
-}
+//   }
+//   return value
+// }
+
+// const App = () =>{
+//   const [name,setName] = useState("")
+//   const[email,setEmail] = useState("")
+//   // const data = inputHandler(name) due to this app is slow
+
+//   // use memo save data in a cache/always return a value
+//   const data = useMemo(()=>
+//     inputHandler(name),[name]
+// )
+  
+
+//   console.log("rendered");
+  
+//   return <div>
+//     <input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+//     <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+//     <p>{data}</p>
+//   </div>
+// }
+
+// export default App
+
+
+// ---------------------------Use Call baxk---------------------------------
+import { useEffect, useState,useCallback} from "react"
 
 const App = () =>{
   const [name,setName] = useState("")
-  const[email,setEmail] = useState("")
-  // const data = inputHandler(name) due to this app is slow
+   const[email,setEmail] = useState("")
+   const[result,setResult] = useState("")
 
-  // use memo save data in a cache/always return a value
-  const data = useMemo(()=>
-    inputHandler(name),[name]
-)
-  
 
-  console.log("rendered");
-  
-  return <div>
-    <input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
-    <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-    <p>{data}</p>
-  </div>
+  //  component rerender - function is recreated
+  //  const myFunction = () => {
+  //   console.log("Function is called");
+  //   return "hello"
+    
+  //  }
+
+  const myFunction = useCallback(() => {
+    console.log("Function is running");
+    return "hello"
+  },[])
+
+   useEffect(() => {
+    console.log("use effect is called");
+    setResult(myFunction())},[myFunction])
+
+    return (
+      <div>
+       <input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+       <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <p>{result}</p>
+      </div>
+    )
+
 }
 
 export default App
